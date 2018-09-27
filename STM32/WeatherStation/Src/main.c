359fb739eca9fba1cd0454c6aa0c988d96e7988d
DionLegierse <dal.legierse@student.avans.nl>
2018-09-27 13:18:39 +0200

    Working Weatherstation readings of Temperature, Humidity and Pressure at the same time with FreeRTOS. Code still needs to be cleaned up and string functions for humidity and temperature need to be fixed.

--- STM32/WeatherStation/Src/main.c
+++ STM32/WeatherStation/Src/main.c
@@ -63,8 +63,9 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
-osThreadId defaultTaskHandle;
+osThreadId taskTemperatureHandle;
 osThreadId taskHumidityHandle;
+osThreadId taskPressureHandle;
 osMutexId I2CBusMutexHandle;
 osMutexId UART2BusMutexHandle;
 
@@ -79,8 +80,9 @@ static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
-void StartDefaultTask(void const * argument);
-void humidityStart(void const * argument);
+void StartTaskTemperature(void const * argument);
+void startTaskHumidity(void const * argument);
+void startTaskPressure(void const * argument);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
@@ -148,13 +150,18 @@ int main(void) {
 	/* USER CODE END RTOS_TIMERS */
 
 	/* Create the thread(s) */
-	/* definition and creation of defaultTask */
-	osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
-	defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+	/* definition and creation of taskTemperature */
+	osThreadDef(taskTemperature, StartTaskTemperature, osPriorityNormal, 0,
+			128);
+	taskTemperatureHandle = osThreadCreate(osThread(taskTemperature), NULL);
 
 	/* definition and creation of taskHumidity */
-	osThreadDef(taskHumidity, humidityStart, osPriorityIdle, 0, 128);
+	osThreadDef(taskHumidity, startTaskHumidity, osPriorityIdle, 0, 128);
 	taskHumidityHandle = osThreadCreate(osThread(taskHumidity), NULL);
+
+	/* definition and creation of taskPressure */
+	osThreadDef(taskPressure, startTaskPressure, osPriorityIdle, 0, 128);
+	taskPressureHandle = osThreadCreate(osThread(taskPressure), NULL);
 
 	/* USER CODE BEGIN RTOS_THREADS */
 	/* add threads, ... */
@@ -347,13 +354,198 @@ static void MX_GPIO_Init(void) {
 
 /* USER CODE BEGIN 4 */
 #define STANDARD_TIMEOUT (uint8_t)100
-#define UART_MUTEX_TIMEOUT (uint8_t)30
+#define UART_MUTEX_TIMEOUT (uint8_t)100
 #define STANDARD_OS_DELAY (uint32_t)1000
 #define ADDRESS_OF_is7021 (uint8_t) 0x40 << 1
 #define MSB_OFFSET (uint8_t) 8
+#define STANDARD_MUTEX_TAKE_TIME (uint32_t)100
+#define PRESSURE_MUTEX_TAKE_TIME (uint32_t)500
+#define MAX_STRING_LENGTH (uint8_t)80
 
 #define ABORTED (uint8_t) 0
 #define SUCCES (uint8_t) 1
+
+#define BUFFER_SIZE_CALIBRATION_DATA (uint8_t)24
+#define FORCE_MODE (uint8_t)0b00100101
+#define MEMORY_NOT_READY_FLAG (uint8_t)0x8
+#define BUFFER_SIZE_RAW_DATA (uint8_t)6
+#define MSB_SHIFT (uint8_t)12
+#define LSB_SHIFT (uint8_t)4
+#define XLSB_SHIFT (uint8_t)4
+#define INVALID_PRESSURE (double)0
+#define PASCAL_TO_MILLIBAR_DIVISION_FACTOR (double)100
+
+//Custom data structures for BMP280
+typedef struct {
+	uint16_t dig_T1;
+	int16_t dig_T2;
+	int16_t dig_T3;
+	uint16_t dig_P1;
+	int16_t dig_P2;
+	int16_t dig_P3;
+	int16_t dig_P4;
+	int16_t dig_P5;
+	int16_t dig_P6;
+	int16_t dig_P7;
+	int16_t dig_P8;
+	int16_t dig_P9;
+} calibrationData;
+
+typedef struct {
+	uint8_t tMSB;
+	uint8_t tLSB;
+	uint8_t tXLSB;
+	uint8_t pMSB;
+	uint8_t pLSB;
+	uint8_t pXLSB;
+} rawData;
+
+typedef struct {
+	uint32_t parsedPressureData;
+	uint32_t parsedTemperatureData;
+	int32_t t_fine;
+	double temperature;
+	double pressure;
+} parsedData;
+
+void getCalibrationData(uint16_t slaveAddress, uint16_t calibrationDataAddress,
+		calibrationData * data) {
+	uint8_t calibBuffer[BUFFER_SIZE_CALIBRATION_DATA];
+
+	HAL_I2C_Mem_Read(&hi2c1, slaveAddress, calibrationDataAddress, sizeof(uint8_t), calibBuffer, BUFFER_SIZE_CALIBRATION_DATA,STANDARD_TIMEOUT);
+
+	data->dig_T1 = ((uint16_t) calibBuffer[1] << 8) | calibBuffer[0];
+	data->dig_T2 = ((uint16_t) calibBuffer[3] << 8) | calibBuffer[2];
+	data->dig_T3 = ((uint16_t) calibBuffer[5] << 8) | calibBuffer[4];
+	data->dig_P1 = ((uint16_t) calibBuffer[7] << 8) | calibBuffer[6];
+	data->dig_P2 = ((uint16_t) calibBuffer[9] << 8) | calibBuffer[8];
+	data->dig_P3 = ((uint16_t) calibBuffer[11] << 8) | calibBuffer[10];
+	data->dig_P4 = ((uint16_t) calibBuffer[13] << 8) | calibBuffer[12];
+	data->dig_P5 = ((uint16_t) calibBuffer[15] << 8) | calibBuffer[14];
+	data->dig_P6 = ((uint16_t) calibBuffer[17] << 8) | calibBuffer[16];
+	data->dig_P7 = ((uint16_t) calibBuffer[19] << 8) | calibBuffer[18];
+	data->dig_P8 = ((uint16_t) calibBuffer[21] << 8) | calibBuffer[20];
+	data->dig_P9 = ((uint16_t) calibBuffer[23] << 8) | calibBuffer[22];
+}
+
+void forceBMP280Measurement(uint16_t slaveAddress, uint16_t modeRegisterAddress,
+		uint16_t statusRegisterAddress) {
+	uint8_t buffer;
+	uint8_t modeData = FORCE_MODE;
+	HAL_I2C_Mem_Write(&hi2c1, slaveAddress, modeRegisterAddress, sizeof(uint8_t), &modeData, sizeof(buffer), STANDARD_TIMEOUT);
+	do {
+		HAL_I2C_Mem_Read(&hi2c1, slaveAddress, statusRegisterAddress, sizeof(uint8_t), &buffer, sizeof(buffer), STANDARD_TIMEOUT);
+	} while (buffer & MEMORY_NOT_READY_FLAG );
+}
+
+void getRawMeasurmentData(uint16_t slaveAddress,
+		uint16_t rawDataRegisterAddress, rawData * data) {
+	uint8_t dataBuffer[BUFFER_SIZE_RAW_DATA ];
+
+	HAL_I2C_Mem_Read(&hi2c1, slaveAddress, rawDataRegisterAddress, sizeof(uint8_t), dataBuffer, sizeof(dataBuffer) / sizeof(uint8_t), STANDARD_TIMEOUT);
+
+	data->pMSB = dataBuffer[0];
+	data->pLSB = dataBuffer[1];
+	data->pXLSB = dataBuffer[2];
+	data->tMSB = dataBuffer[3];
+	data->tLSB = dataBuffer[4];
+	data->tXLSB = dataBuffer[5];
+}
+
+void parseData(rawData * raw, parsedData * data) {
+	uint32_t lsb;
+	uint32_t msb;
+	uint32_t xlsb;
+
+	msb = (uint32_t) raw->pMSB << MSB_SHIFT;
+	lsb = (uint32_t) raw->pLSB << LSB_SHIFT;
+	xlsb = (uint32_t) raw->pXLSB >> XLSB_SHIFT;
+	data->parsedPressureData = msb | lsb | xlsb;
+
+	msb = (uint32_t) raw->tMSB << MSB_SHIFT;
+	lsb = (uint32_t) raw->tLSB << LSB_SHIFT;
+	xlsb = (uint32_t) raw->tXLSB >> XLSB_SHIFT;
+	data->parsedTemperatureData = msb | lsb | xlsb;
+}
+
+void calculateTemperature(parsedData * parsed, calibrationData * calibData) {
+	double var1;
+	double var2;
+
+	var1 = ((double) parsed->parsedTemperatureData) / 16384.0
+			- ((double) calibData->dig_T1) / 1024.0;
+	var1 = var1 * ((double) calibData->dig_T2);
+	var2 = (((double) parsed->parsedTemperatureData) / 131072.0
+			- ((double) calibData->dig_T1) / 8192.0);
+	var2 = (var2 * var2) * ((double) calibData->dig_T3);
+	parsed->t_fine = (int32_t) (var1 + var2);
+	parsed->temperature = (var1 + var2) / 5120.0;
+}
+
+void calculatePressure(parsedData * parsed, calibrationData * calibData) {
+	double var1;
+	double var2;
+	double var3;
+	double pressure;
+
+	var1 = ((double) parsed->t_fine / 2.0) - 64000.0;
+	var2 = var1 * var1 * ((double) calibData->dig_P6) / 32768.0;
+	var2 = var2 + var1 * ((double) calibData->dig_P5) * 2.0;
+	var2 = (var2 / 4.0) + (((double) calibData->dig_P4) * 65536.0);
+	var3 = ((double) calibData->dig_P3) * var1 * var1 / 524288.0;
+	var1 = (var3 + ((double) calibData->dig_P2) * var1) / 524288.0;
+	var1 = (1.0 + var1 / 32768.0) * ((double) calibData->dig_P1);
+	/* avoid exception caused by division by zero */
+	if (var1) {
+		pressure = 1048576.0 - (double) parsed->parsedPressureData;
+		pressure = (pressure - (var2 / 4096.0)) * 6250.0 / var1;
+		var1 = ((double) calibData->dig_P9) * pressure * pressure
+				/ 2147483648.0;
+		var2 = pressure * ((double) calibData->dig_P8) / 32768.0;
+		pressure = pressure
+				+ (var1 + var2 + ((double) calibData->dig_P7)) / 16.0;
+
+	} else { /* Invalid case */
+		pressure = INVALID_PRESSURE;
+	}
+
+	parsed->pressure = pressure / PASCAL_TO_MILLIBAR_DIVISION_FACTOR;
+}
+
+double getPressure() {
+	uint16_t modeRegisterAddress = 0xF4;
+	uint16_t statusRegisterAddress = 0xF3;
+	uint16_t slaveAddressBMP280 = 0x77 << 1;
+	uint16_t rawDataRegisterAddress = 0xF7;
+	uint16_t startAddressCalibData = 0x88;
+
+	calibrationData calibData;
+	parsedData parsed;
+	rawData raw;
+
+	xSemaphoreTake(I2CBusMutexHandle, PRESSURE_MUTEX_TAKE_TIME);
+	// Get calibration data
+	getCalibrationData(slaveAddressBMP280, startAddressCalibData, &calibData);
+
+	// Force sensor to measure and wait for sensor to be done
+	forceBMP280Measurement(slaveAddressBMP280, modeRegisterAddress,
+			statusRegisterAddress);
+
+	//get raw data for calculations
+	getRawMeasurmentData(slaveAddressBMP280, rawDataRegisterAddress, &raw);
+	xSemaphoreGive(I2CBusMutexHandle);
+	// Parse data to make ready for calculation and delete raw data
+	parseData(&raw, &parsed);
+
+	// Calculate temperature
+	calculateTemperature(&parsed, &calibData);
+
+	// Calculate pressure
+	calculatePressure(&parsed, &calibData);
+
+	//return parsed.pressure;
+	return parsed.pressure;
+}
 
 /*
  INPUT: requires pointer to a buffer to store the two data bytes, a pointer to the command to be send
@@ -364,16 +556,14 @@ static void MX_GPIO_Init(void) {
 uint8_t get_data_from_is7021(uint8_t * buffer, uint8_t * command,
 		uint8_t bufferSize) {
 	if (bufferSize < 2)
-		return ABORTED;
+		return ABORTED ;
 	else {
-		xSemaphoreTake(I2CBusMutexHandle, 30);
-		HAL_I2C_Master_Transmit(&hi2c1, ADDRESS_OF_is7021, command, 1,
-				STANDARD_TIMEOUT);
+		xSemaphoreTake(I2CBusMutexHandle, STANDARD_MUTEX_TAKE_TIME);
+		HAL_I2C_Master_Transmit(&hi2c1, ADDRESS_OF_is7021, command, 1, STANDARD_TIMEOUT);
 		osDelay(23);
-		HAL_I2C_Master_Receive(&hi2c1, ADDRESS_OF_is7021, buffer, 2,
-		STANDARD_TIMEOUT);
+		HAL_I2C_Master_Receive(&hi2c1, ADDRESS_OF_is7021, buffer, 2, STANDARD_TIMEOUT);
 		xSemaphoreGive(I2CBusMutexHandle);
-		return SUCCES;
+		return SUCCES ;
 	}
 
 }
@@ -389,7 +579,7 @@ uint8_t get_data_from_is7021(uint8_t * buffer, uint8_t * command,
  */
 uint8_t convert_data_to_temp_string(uint8_t * buffer, uint8_t bufferLength) {
 	if (bufferLength < 4)
-		return ABORTED;
+		return ABORTED ;
 	else {
 		uint16_t data = buffer[0] << MSB_OFFSET | buffer[1];
 		int tempratureTimesTen = (int) roundf(
@@ -398,7 +588,7 @@ uint8_t convert_data_to_temp_string(uint8_t * buffer, uint8_t bufferLength) {
 		buffer[bufferLength - 1] = buffer[2];
 		buffer[2] = '.';
 		buffer[3] = buffer[bufferLength - 1];
-		return SUCCES;
+		return SUCCES ;
 	}
 }
 
@@ -412,18 +602,18 @@ uint8_t convert_data_to_temp_string(uint8_t * buffer, uint8_t bufferLength) {
  */
 uint8_t convert_data_to_humid_string(uint8_t * buffer, uint8_t bufferLength) {
 	if (bufferLength < 2)
-		return ABORTED;
+		return ABORTED ;
 	else {
 		uint16_t data = buffer[0] << MSB_OFFSET | buffer[1];
 		int humidTimesTen = (int) roundf(((((data * 125) / 65536) - 6) * 100));
 		itoa(humidTimesTen, (char *) buffer, 10);
-		return SUCCES;
+		return SUCCES ;
 	}
 }
 /* USER CODE END 4 */
 
-/* StartDefaultTask function */
-void StartDefaultTask(void const * argument) {
+/* StartTaskTemperature function */
+void StartTaskTemperature(void const * argument) {
 
 	/* USER CODE BEGIN 5 */
 
@@ -448,12 +638,11 @@ void StartDefaultTask(void const * argument) {
 	/* USER CODE END 5 */
 }
 
-/* humidityStart function */
-void humidityStart(void const * argument) {
-	/* USER CODE BEGIN humidityStart */
-	const uint8_t bufferSize = 4;
-
+/* startTaskHumidity function */
+void startTaskHumidity(void const * argument) {
+	const uint8_t bufferSize = 2;
 	uint8_t commandLoadHumidity = 0xE5;
+
 	uint8_t buffer[bufferSize];
 
 	/* Infinite loop */
@@ -463,14 +652,31 @@ void humidityStart(void const * argument) {
 		convert_data_to_humid_string(buffer, bufferSize);
 
 		xSemaphoreTake(UART2BusMutexHandle, UART_MUTEX_TIMEOUT);
-		HAL_UART_Transmit(&huart2, buffer, 2, STANDARD_TIMEOUT);
-		HAL_UART_Transmit(&huart2, (uint8_t *) " \% Humid\n", 9,
-		STANDARD_TIMEOUT);
+		HAL_UART_Transmit(&huart2, buffer, bufferSize, STANDARD_TIMEOUT);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "\% humid\n", 8,
+				STANDARD_TIMEOUT);
 		xSemaphoreGive(UART2BusMutexHandle);
-
 		osDelay(STANDARD_OS_DELAY);
 	}
-	/* USER CODE END humidityStart */
+}
+
+/* startTaskPressure function */
+void startTaskPressure(void const * argument) {
+	/* USER CODE BEGIN startTaskPressure */
+	/* Infinite loop */
+	const uint8_t bufferSize = 20;
+	uint8_t buffer[bufferSize];
+	memset(buffer, '\n', bufferSize);
+
+	for (;;) {
+		double pressure = getPressure();
+		sprintf(buffer, "%d millibar\n", (uint32_t)pressure);
+		xSemaphoreTake(UART2BusMutexHandle, UART_MUTEX_TIMEOUT);
+		HAL_UART_Transmit(&huart2, buffer, (uint8_t)strlen(buffer), STANDARD_TIMEOUT);
+		xSemaphoreGive(UART2BusMutexHandle);
+		osDelay(STANDARD_OS_DELAY);
+	}
+	/* USER CODE END startTaskPressure */
 }
 
 /**
