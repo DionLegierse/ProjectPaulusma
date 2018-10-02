f62afecf42fa0efad30e84afbb6bc8845a25a896
DionLegierse <dal.legierse@student.avans.nl>
2018-10-02 14:44:53 +0200

    Cleaned up code by adding a .c and .h file for the functions

--- STM32/WeatherStation/Src/main.c
+++ STM32/WeatherStation/Src/main.c
@@ -56,6 +56,8 @@
 #include "math.h"
 #include "stdio.h"
 #include "stdlib.h"
+
+#include "measurementFunctions.h"
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
@@ -373,253 +375,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-#define STANDARD_TIMEOUT (uint8_t)100
-#define UART_MUTEX_TIMEOUT (uint8_t)100
-#define STANDARD_OS_DELAY (uint32_t)1000
-#define ADDRESS_OF_is7021 (uint8_t) 0x40 << 1
-#define MSB_OFFSET (uint8_t) 8
-#define STANDARD_MUTEX_TAKE_TIME (uint32_t)100
-#define PRESSURE_MUTEX_TAKE_TIME (uint32_t)500
-#define MAX_BUFFER_SIZE (uint8_t)40
-
-#define ABORTED (uint8_t) 0
-#define SUCCES (uint8_t) 1
-#define READY (uint8_t)1
-#define NOT_READY (uint8_t)0
-
-#define BUFFER_SIZE_CALIBRATION_DATA (uint8_t)24
-#define FORCE_MODE (uint8_t)0b00100101
-#define MEMORY_NOT_READY_FLAG (uint8_t)0x8
-#define BUFFER_SIZE_RAW_DATA (uint8_t)6
-#define MSB_SHIFT (uint8_t)12
-#define LSB_SHIFT (uint8_t)4
-#define XLSB_SHIFT (uint8_t)4
-#define INVALID_PRESSURE (double)0
-#define PASCAL_TO_MILLIBAR_DIVISION_FACTOR (double)100
-
-//Custom data structures for BMP280
-typedef struct {
-	uint16_t dig_T1;
-	int16_t dig_T2;
-	int16_t dig_T3;
-	uint16_t dig_P1;
-	int16_t dig_P2;
-	int16_t dig_P3;
-	int16_t dig_P4;
-	int16_t dig_P5;
-	int16_t dig_P6;
-	int16_t dig_P7;
-	int16_t dig_P8;
-	int16_t dig_P9;
-} calibrationData;
-
-typedef struct {
-	uint8_t tMSB;
-	uint8_t tLSB;
-	uint8_t tXLSB;
-	uint8_t pMSB;
-	uint8_t pLSB;
-	uint8_t pXLSB;
-} rawData;
-
-typedef struct {
-	uint32_t parsedPressureData;
-	uint32_t parsedTemperatureData;
-	int32_t t_fine;
-	double temperature;
-	double pressure;
-} parsedData;
-
-void getCalibrationData(uint16_t slaveAddress, uint16_t calibrationDataAddress,
-		calibrationData * data) {
-	uint8_t calibBuffer[BUFFER_SIZE_CALIBRATION_DATA];
-
-	HAL_I2C_Mem_Read(&hi2c1, slaveAddress, calibrationDataAddress, sizeof(uint8_t), calibBuffer, BUFFER_SIZE_CALIBRATION_DATA,STANDARD_TIMEOUT);
-
-	data->dig_T1 = ((uint16_t) calibBuffer[1] << 8) | calibBuffer[0];
-	data->dig_T2 = ((uint16_t) calibBuffer[3] << 8) | calibBuffer[2];
-	data->dig_T3 = ((uint16_t) calibBuffer[5] << 8) | calibBuffer[4];
-	data->dig_P1 = ((uint16_t) calibBuffer[7] << 8) | calibBuffer[6];
-	data->dig_P2 = ((uint16_t) calibBuffer[9] << 8) | calibBuffer[8];
-	data->dig_P3 = ((uint16_t) calibBuffer[11] << 8) | calibBuffer[10];
-	data->dig_P4 = ((uint16_t) calibBuffer[13] << 8) | calibBuffer[12];
-	data->dig_P5 = ((uint16_t) calibBuffer[15] << 8) | calibBuffer[14];
-	data->dig_P6 = ((uint16_t) calibBuffer[17] << 8) | calibBuffer[16];
-	data->dig_P7 = ((uint16_t) calibBuffer[19] << 8) | calibBuffer[18];
-	data->dig_P8 = ((uint16_t) calibBuffer[21] << 8) | calibBuffer[20];
-	data->dig_P9 = ((uint16_t) calibBuffer[23] << 8) | calibBuffer[22];
-}
-
-void forceBMP280Measurement(uint16_t slaveAddress, uint16_t modeRegisterAddress,
-		uint16_t statusRegisterAddress) {
-	uint8_t buffer;
-	uint8_t modeData = FORCE_MODE;
-	HAL_I2C_Mem_Write(&hi2c1, slaveAddress, modeRegisterAddress, sizeof(uint8_t), &modeData, sizeof(buffer), STANDARD_TIMEOUT);
-	do {
-		HAL_I2C_Mem_Read(&hi2c1, slaveAddress, statusRegisterAddress, sizeof(uint8_t), &buffer, sizeof(buffer), STANDARD_TIMEOUT);
-	} while (buffer & MEMORY_NOT_READY_FLAG );
-}
-
-void getRawMeasurmentData(uint16_t slaveAddress,
-		uint16_t rawDataRegisterAddress, rawData * data) {
-	uint8_t dataBuffer[BUFFER_SIZE_RAW_DATA ];
-
-	HAL_I2C_Mem_Read(&hi2c1, slaveAddress, rawDataRegisterAddress, sizeof(uint8_t), dataBuffer, sizeof(dataBuffer) / sizeof(uint8_t), STANDARD_TIMEOUT);
-
-	data->pMSB = dataBuffer[0];
-	data->pLSB = dataBuffer[1];
-	data->pXLSB = dataBuffer[2];
-	data->tMSB = dataBuffer[3];
-	data->tLSB = dataBuffer[4];
-	data->tXLSB = dataBuffer[5];
-}
-
-void parseData(rawData * raw, parsedData * data) {
-	uint32_t lsb;
-	uint32_t msb;
-	uint32_t xlsb;
-
-	msb = (uint32_t) raw->pMSB << MSB_SHIFT;
-	lsb = (uint32_t) raw->pLSB << LSB_SHIFT;
-	xlsb = (uint32_t) raw->pXLSB >> XLSB_SHIFT;
-	data->parsedPressureData = msb | lsb | xlsb;
-
-	msb = (uint32_t) raw->tMSB << MSB_SHIFT;
-	lsb = (uint32_t) raw->tLSB << LSB_SHIFT;
-	xlsb = (uint32_t) raw->tXLSB >> XLSB_SHIFT;
-	data->parsedTemperatureData = msb | lsb | xlsb;
-}
-
-void calculateTemperature(parsedData * parsed, calibrationData * calibData) {
-	double var1;
-	double var2;
-
-	var1 = ((double) parsed->parsedTemperatureData) / 16384.0
-			- ((double) calibData->dig_T1) / 1024.0;
-	var1 = var1 * ((double) calibData->dig_T2);
-	var2 = (((double) parsed->parsedTemperatureData) / 131072.0
-			- ((double) calibData->dig_T1) / 8192.0);
-	var2 = (var2 * var2) * ((double) calibData->dig_T3);
-	parsed->t_fine = (int32_t) (var1 + var2);
-	parsed->temperature = (var1 + var2) / 5120.0;
-}
-
-void calculatePressure(parsedData * parsed, calibrationData * calibData) {
-	double var1;
-	double var2;
-	double var3;
-	double pressure;
-
-	var1 = ((double) parsed->t_fine / 2.0) - 64000.0;
-	var2 = var1 * var1 * ((double) calibData->dig_P6) / 32768.0;
-	var2 = var2 + var1 * ((double) calibData->dig_P5) * 2.0;
-	var2 = (var2 / 4.0) + (((double) calibData->dig_P4) * 65536.0);
-	var3 = ((double) calibData->dig_P3) * var1 * var1 / 524288.0;
-	var1 = (var3 + ((double) calibData->dig_P2) * var1) / 524288.0;
-	var1 = (1.0 + var1 / 32768.0) * ((double) calibData->dig_P1);
-	/* avoid exception caused by division by zero */
-	if (var1) {
-		pressure = 1048576.0 - (double) parsed->parsedPressureData;
-		pressure = (pressure - (var2 / 4096.0)) * 6250.0 / var1;
-		var1 = ((double) calibData->dig_P9) * pressure * pressure
-				/ 2147483648.0;
-		var2 = pressure * ((double) calibData->dig_P8) / 32768.0;
-		pressure = pressure
-				+ (var1 + var2 + ((double) calibData->dig_P7)) / 16.0;
-
-	} else { /* Invalid case */
-		pressure = INVALID_PRESSURE;
-	}
-
-	parsed->pressure = pressure / PASCAL_TO_MILLIBAR_DIVISION_FACTOR;
-}
-
-double getPressure() {
-	uint16_t modeRegisterAddress = 0xF4;
-	uint16_t statusRegisterAddress = 0xF3;
-	uint16_t slaveAddressBMP280 = 0x77 << 1;
-	uint16_t rawDataRegisterAddress = 0xF7;
-	uint16_t startAddressCalibData = 0x88;
-
-	calibrationData calibData;
-	parsedData parsed;
-	rawData raw;
-
-	xSemaphoreTake(I2CBusMutexHandle, PRESSURE_MUTEX_TAKE_TIME);
-	// Get calibration data
-	getCalibrationData(slaveAddressBMP280, startAddressCalibData, &calibData);
-
-	// Force sensor to measure and wait for sensor to be done
-	forceBMP280Measurement(slaveAddressBMP280, modeRegisterAddress,
-			statusRegisterAddress);
-
-	//get raw data for calculations
-	getRawMeasurmentData(slaveAddressBMP280, rawDataRegisterAddress, &raw);
-	xSemaphoreGive(I2CBusMutexHandle);
-	// Parse data to make ready for calculation and delete raw data
-	parseData(&raw, &parsed);
-
-	// Calculate temperature
-	calculateTemperature(&parsed, &calibData);
-
-	// Calculate pressure
-	calculatePressure(&parsed, &calibData);
-
-	//return parsed.pressure;
-	return parsed.pressure;
-}
-
-/*
- INPUT: requires pointer to a buffer to store the two data bytes, a pointer to the command to be send
- and the size of the buffer in bytes.
- OUTPUT: returns a zero (ABORTED) if the buffer was not large enough and a one (SUCCES) if retrieval was succesful
- FUNCTION: gets data of from the is7021
- */
-uint8_t get_data_from_is7021(uint8_t * buffer, uint8_t * command,
-		uint8_t bufferSize) {
-	if (bufferSize < 2)
-		return ABORTED ;
-	else {
-		xSemaphoreTake(I2CBusMutexHandle, STANDARD_MUTEX_TAKE_TIME);
-		HAL_I2C_Master_Transmit(&hi2c1, ADDRESS_OF_is7021, command, 1, STANDARD_TIMEOUT);
-		osDelay(23);
-		HAL_I2C_Master_Receive(&hi2c1, ADDRESS_OF_is7021, buffer, 2, STANDARD_TIMEOUT);
-		xSemaphoreGive(I2CBusMutexHandle);
-		return SUCCES ;
-	}
-
-}
-
-/*
- INPUT: the input of the function requires a pointer to a buffer, with the temprature data
- stored in the first two bytes, and the length of the buffer
- The minimal amount of space required is 4 bytes.
- OUTPUT: returns a zero (ABORTED) if the buffer was not large enough and a one (SUCCES) if conversion was succcesfull
- FUNCTION: this functions takes the first two bytes in the buffer and
- converts them to a string and stores this string in the first three bytes of the buffer.
- The buffer always requires a storage space in the back for a point within the string.
- */
-uint16_t convert_data_temp_to_int(uint8_t * buffer, uint8_t bufferLength) {
-	uint16_t data = buffer[0] << MSB_OFFSET | buffer[1];
-	return (uint16_t) roundf(((((data * 175.72) / 65536) - 46.85) * 10));
-}
-
-/*
- INPUT: the input of the function requires a pointer to a buffer, with the humidity data
- stored in the first two bytes, and the length of the buffer
- The minimal amount of space required in the buffer is 2 bytes.
- OUTPUT: returns a zero (ABORTED) if the buffer was not large enough and a one (SUCCES) if conversion was succcesfull
- FUNCTION: this functions takes the first two bytes in the buffer and
- converts them to a string and stores this string in the first two bytes of the buffer.
- */
-uint16_t convert_data_humid_to_int(uint8_t * buffer, uint8_t bufferLength) {
-	uint16_t data = buffer[0] << MSB_OFFSET | buffer[1];
-	return (uint16_t) roundf(((((data * 125) / 65536) - 6)));
-}
-
-/* USER CODE END 4 */
-
-//Global variables
 uint16_t globalPressure = 0;
 uint16_t globalTemperature = 0;
 uint16_t globalHumidity = 0;
@@ -627,6 +382,10 @@ uint16_t globalHumidity = 0;
 uint8_t isPressureDone = NOT_READY;
 uint8_t isTemperatureDone = NOT_READY;
 uint8_t isHumidityDoneFlag = NOT_READY;
+/* USER CODE END 4 */
+
+//Global variables
+
 
 /* StartTaskTemperature function */
 void StartTaskTemperature(void const * argument)
@@ -634,13 +393,13 @@ void StartTaskTemperature(void const * argument)
 
   /* USER CODE BEGIN 5 */
 	uint8_t commandLoadTemp = 0xE3;
-
 	uint8_t buffer[ MAX_BUFFER_SIZE];
 
 	/* Infinite loop */
 	for (;;) {
-		get_data_from_is7021(buffer, &commandLoadTemp, MAX_BUFFER_SIZE);
-		globalTemperature = convert_data_temp_to_int(buffer, MAX_BUFFER_SIZE);
+		get_data_from_is7021(buffer, &commandLoadTemp);
+
+		globalTemperature = convert_data_temp_to_int(buffer);
 		isTemperatureDone = READY;
 		vTaskSuspend(taskTemperatureHandle);
 	}
@@ -656,9 +415,9 @@ void startTaskHumidity(void const * argument)
 
 	/* Infinite loop */
 	for (;;) {
-		get_data_from_is7021(buffer, &commandLoadHumidity, MAX_BUFFER_SIZE);
+		get_data_from_is7021(buffer, &commandLoadHumidity);
 
-		globalHumidity = convert_data_humid_to_int(buffer, MAX_BUFFER_SIZE);
+		globalHumidity = convert_data_humid_to_int(buffer);
 
 		isHumidityDoneFlag = READY;
 		vTaskSuspend(taskHumidityHandle);
@@ -670,11 +429,9 @@ void startTaskPressure(void const * argument)
 {
   /* USER CODE BEGIN startTaskPressure */
 	/* Infinite loop */
-	uint8_t buffer[MAX_BUFFER_SIZE];
-	memset(buffer, '\n', MAX_BUFFER_SIZE);
-
 	for (;;) {
 		globalPressure = (uint16_t)getPressure();
+
 		isPressureDone = READY;
 		vTaskSuspend(taskPressureHandle);
 	}
@@ -713,7 +470,7 @@ void startTaskWifi(void const * argument)
 		  vTaskResume(taskPressureHandle);
 		  vTaskResume(taskHumidityHandle);
 
-		  osDelay(50000);
+		  osDelay(MEASURE_DELAY);
 	  }
   }
   /* USER CODE END startTaskWifi */
